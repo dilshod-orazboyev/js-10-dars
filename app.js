396c8b464f789fa40a82b51a4577a836659627d3
Javohir Narimonov <javohirnarimonov2009@gmail.com>
2026-02-18 17:19:51 +0500

    object ishladik

---
+++ app.js
@@ -0,0 +1,13 @@
+// Oblect --> Array, Function
+
+const students = ["Bmw", "Bugatti","Posche", "Pagani", "Mers musr", "Cobalt", "Golf"]
+console.log(students[0]); // index orqali ulanish
+console.log(students.length); // array uzunligini qaytaradi
+console.log(students[students.length - 1]); // arrayning oxirgi elementini qaytaradi
+
+
+
+// Index yordamida array elementlarini o'zgartirish
+students[4] = "Gentra"
+console.log(students);
+
